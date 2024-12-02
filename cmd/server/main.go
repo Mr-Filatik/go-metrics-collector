13be71327917bd32f6c3081df9416e410785979b
Vladislav Filatov <vladislav.filatov@mego.travel>
2024-12-02 16:33:17 +0400

    Fix create metric

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,43 +15,6 @@ var stor storages.Storage = &storages.MemStorage{}
 
 func main() {
 	endpoint := "127.0.0.1:8080"
-	stor.Create(analiticmetrics.Gauge, "testGauge", "0")
-	stor.Create(analiticmetrics.Counter, "testCounter", "0")
-	stor.Create(analiticmetrics.Counter, "testSetGet54", "0")
-	stor.Create(analiticmetrics.Counter, "testSetGet207", "0")
-	stor.Create(analiticmetrics.Gauge, "testSetGet106", "0")
-	stor.Create(analiticmetrics.Gauge, "testSetGet172", "0")
-	stor.Create(analiticmetrics.Gauge, "testSetGet207", "0")
-
-	stor.Create(analiticmetrics.Gauge, "Alloc", "0")
-	stor.Create(analiticmetrics.Gauge, "BuckHashSys", "0")
-	stor.Create(analiticmetrics.Gauge, "Frees", "0")
-	stor.Create(analiticmetrics.Gauge, "GCCPUFraction", "0")
-	stor.Create(analiticmetrics.Gauge, "GCSys", "0")
-	stor.Create(analiticmetrics.Gauge, "HeapAlloc", "0")
-	stor.Create(analiticmetrics.Gauge, "HeapIdle", "0")
-	stor.Create(analiticmetrics.Gauge, "HeapInuse", "0")
-	stor.Create(analiticmetrics.Gauge, "HeapObjects", "0")
-	stor.Create(analiticmetrics.Gauge, "HeapReleased", "0")
-	stor.Create(analiticmetrics.Gauge, "HeapSys", "0")
-	stor.Create(analiticmetrics.Gauge, "LastGC", "0")
-	stor.Create(analiticmetrics.Gauge, "Lookups", "0")
-	stor.Create(analiticmetrics.Gauge, "MCacheInuse", "0")
-	stor.Create(analiticmetrics.Gauge, "MCacheSys", "0")
-	stor.Create(analiticmetrics.Gauge, "MSpanInuse", "0")
-	stor.Create(analiticmetrics.Gauge, "MSpanSys", "0")
-	stor.Create(analiticmetrics.Gauge, "Mallocs", "0")
-	stor.Create(analiticmetrics.Gauge, "NextGC", "0")
-	stor.Create(analiticmetrics.Gauge, "NumForcedGC", "0")
-	stor.Create(analiticmetrics.Gauge, "NumGC", "0")
-	stor.Create(analiticmetrics.Gauge, "OtherSys", "0")
-	stor.Create(analiticmetrics.Gauge, "PauseTotalNs", "0")
-	stor.Create(analiticmetrics.Gauge, "StackInuse", "0")
-	stor.Create(analiticmetrics.Gauge, "StackSys", "0")
-	stor.Create(analiticmetrics.Gauge, "Sys", "0")
-	stor.Create(analiticmetrics.Gauge, "TotalAlloc", "0")
-	stor.Create(analiticmetrics.Counter, "PollCount", "0")
-	stor.Create(analiticmetrics.Gauge, "RandomValue", "0")
 
 	r := chi.NewRouter()
 	r.Get("/", allInfoHandle)
@@ -124,7 +87,7 @@ func updateHandle(w http.ResponseWriter, r *http.Request) {
 	switch t {
 	case analiticmetrics.Gauge:
 		if !stor.Contains(t, n) {
-			http.Error(w, "Incorrect metric name", http.StatusNotFound)
+			stor.Create(analiticmetrics.Gauge, n, v)
 			return
 		}
 		if err := stor.Update(t, n, v); err != nil {
@@ -133,7 +96,7 @@ func updateHandle(w http.ResponseWriter, r *http.Request) {
 		}
 	case analiticmetrics.Counter:
 		if !stor.Contains(t, n) {
-			http.Error(w, "Incorrect metric name", http.StatusNotFound)
+			stor.Create(analiticmetrics.Counter, n, v)
 			return
 		}
 		if err := stor.Update(t, n, v); err != nil {
