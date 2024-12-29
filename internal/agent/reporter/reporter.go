85d8dd1062636dc54964890a6ab93ee2d03bc7e1
Vladislav Filatov <vladislav.filatov@mego.travel>
2024-12-29 13:33:25 +0400

    Create compress to request

--- internal/agent/reporter/reporter.go
+++ internal/agent/reporter/reporter.go
@@ -1,9 +1,14 @@
 package reporter
 
 import (
+	"bytes"
+	"compress/gzip"
 	"encoding/json"
+	"errors"
+	"io"
 	"log"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/Mr-Filatik/go-metrics-collector/internal/agent/metric"
@@ -16,34 +21,75 @@ func Run(m *metric.AgentMetrics, endpoint string, reportInterval int64) {
 
 	for range t {
 		client := resty.New()
-		for _, el := range m.GetAllGauge() {
-			if num, err := strconv.ParseFloat(el.Value, 64); err == nil {
-				metr := entity.Metrics{
-					ID:    el.Name,
-					MType: el.Type,
-					Value: &num,
-				}
 
-				dat, rerr := json.Marshal(metr)
-				if rerr != nil {
-					log.Printf("Error on json create: %v.", rerr.Error())
-					continue
+		client.OnBeforeRequest(func(c *resty.Client, r *resty.Request) error {
+			if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+				body := r.Body
+				if body != nil {
+					compressedBody, err := compressBody(body.([]byte))
+					if err != nil {
+						log.Printf("Compress body error: %v", err.Error())
+					} else {
+						log.Printf("Compress body: %v -> %v", len(body.([]byte)), len(compressedBody))
+						r.SetBody(compressedBody)
+					}
+				} else {
+					log.Printf("Compress body error: body is empty")
 				}
-
-				address := endpoint + "/update/"
-				log.Printf("Response to %v. (%v, %v, %v, %v)", address, metr.ID, metr.MType, *metr.Value, metr.Delta)
-				resp, err := client.R().
-					SetHeader("Content-Type", " application/json").
-					SetBody(dat).
-					Post(address)
-
-				if err != nil {
-					log.Printf("Error on response: %v.", err.Error())
-					continue
-				}
-				log.Printf("Response is done. StatusCode: %v. Data: %v.", resp.Status(), string(resp.Body()))
 			}
-		}
+			return nil
+		})
+
+		client.OnAfterResponse(func(c *resty.Client, r *resty.Response) error {
+			for name, values := range r.Header() {
+				for _, value := range values {
+					log.Printf("Response header: %v: %v", name, value)
+				}
+			}
+			if strings.Contains(r.Header().Get("Content-Encoding"), "gzip") {
+				val, err := decompressBody(r.Body())
+				if err != nil {
+					if err.Error() == "gzip: invalid header" {
+						log.Printf("Decompress body error: body not compress")
+					} else {
+						log.Printf("Decompress body error: %v", err.Error())
+					}
+				} else {
+					log.Printf("Decompress body: %v -> %v", len(r.Body()), len(val))
+					r.SetBody(val)
+				}
+			}
+			return nil
+		})
+
+		// for _, el := range m.GetAllGauge() {
+		// 	if num, err := strconv.ParseFloat(el.Value, 64); err == nil {
+		// 		metr := entity.Metrics{
+		// 			ID:    el.Name,
+		// 			MType: el.Type,
+		// 			Value: &num,
+		// 		}
+
+		// 		dat, rerr := json.Marshal(metr)
+		// 		if rerr != nil {
+		// 			log.Printf("Error on json create: %v.", rerr.Error())
+		// 			continue
+		// 		}
+
+		// 		address := endpoint + "/update/"
+		// 		log.Printf("Response to %v. (%v, %v, %v, %v)", address, metr.ID, metr.MType, *metr.Value, metr.Delta)
+		// 		resp, err := client.R().
+		// 			SetHeader("Content-Type", " application/json").
+		// 			SetBody(dat).
+		// 			Post(address)
+
+		// 		if err != nil {
+		// 			log.Printf("Error on response: %v.", err.Error())
+		// 			continue
+		// 		}
+		// 		log.Printf("Response is done. StatusCode: %v. Data: %v.", resp.Status(), string(resp.Body()))
+		// 	}
+		// }
 
 		for _, el := range m.GetAllCounter() {
 			met := m.GetCounter(el)
@@ -54,9 +100,9 @@ func Run(m *metric.AgentMetrics, endpoint string, reportInterval int64) {
 					Delta: &num,
 				}
 
-				dat, rerr := json.Marshal(metr)
-				if rerr != nil {
-					log.Printf("Error on json create: %v.", rerr.Error())
+				dat, err := json.Marshal(metr)
+				if err != nil {
+					log.Printf("Error on json create: %v.", err.Error())
 					continue
 				}
 
@@ -64,6 +110,8 @@ func Run(m *metric.AgentMetrics, endpoint string, reportInterval int64) {
 				log.Printf("Response to %v. (%v, %v, %v, %v)", address, metr.ID, metr.MType, metr.Value, *metr.Delta)
 				resp, rerr := client.R().
 					SetHeader("Content-Type", " application/json").
+					SetHeader("Content-Encoding", "gzip").
+					SetHeader("Accept-Encoding", "gzip").
 					SetBody(dat).
 					Post(address)
 
@@ -71,9 +119,51 @@ func Run(m *metric.AgentMetrics, endpoint string, reportInterval int64) {
 					log.Printf("Error on response: %v.", rerr.Error())
 					continue
 				}
+
+				// if resp.Request.Header.Get("Accept-Encoding") == "gzip" {
+				// 	log.Printf("Body len: %v.", len(resp.Body()))
+				// 	result, err := decompressBody(resp.Body())
+				// 	if err != nil {
+				// 		log.Printf("Error on decompress body: %v.", err.Error())
+				// 		continue
+				// 	}
+				// 	m.ClearCounter(el)
+				// 	log.Printf("Response is done. StatusCode: %v. Data: %v.", resp.Status(), string(result))
+				// 	continue
+				// }
+
 				m.ClearCounter(el)
 				log.Printf("Response is done. StatusCode: %v. Data: %v.", resp.Status(), string(resp.Body()))
 			}
 		}
 	}
+}
+
+func compressBody(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	gw := gzip.NewWriter(&buf)
+	_, gerr := gw.Write(data)
+	if gerr != nil {
+		return nil, errors.New(gerr.Error())
+	}
+	if err := gw.Close(); err != nil {
+		return nil, errors.New(err.Error())
+	}
+	return buf.Bytes(), nil
+}
+
+func decompressBody(data []byte) ([]byte, error) {
+	gr, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, gr)
+	if err != nil {
+		return nil, err
+	}
+	if err := gr.Close(); err != nil {
+		return nil, errors.New(err.Error())
+	}
+	return buf.Bytes(), nil
 }
