0e16b8b72a7688da394957960c64c36b2ea668f4
Mr-Filatik <filatov23wlad@gmail.com>
2025-03-16 22:48:52 +0400

    Revert "Create compress to request"
    This reverts commit 85d8dd1062636dc54964890a6ab93ee2d03bc7e1.

--- internal/server/middleware/middleware.go
+++ internal/server/middleware/middleware.go
@@ -1,10 +1,7 @@
 package middleware
 
 import (
-	"compress/gzip"
-	"io"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/Mr-Filatik/go-metrics-collector/internal/logger"
@@ -25,7 +22,7 @@ func New(l logger.Logger) *Conveyor {
 type Middleware func(http.Handler) http.Handler
 
 func (c *Conveyor) MainConveyor(h http.Handler) http.Handler {
-	return c.registerConveyor(h, c.WithLogging, c.WithGzipSupport)
+	return c.registerConveyor(h, c.WithLogging)
 }
 
 func (c *Conveyor) registerConveyor(h http.Handler, ms ...Middleware) http.Handler {
@@ -54,7 +51,6 @@ func (c *Conveyor) WithLogging(next http.Handler) http.Handler {
 			"request_uri", r.RequestURI,
 			"request_time", startTime.String(),
 			"request_duration", time.Since(startTime),
-			"content_lenght", r.ContentLength,
 		)
 
 		statusCode := lwr.Status()
@@ -69,68 +65,4 @@ func (c *Conveyor) WithLogging(next http.Handler) http.Handler {
 			"content_lenght", lwr.Size(),
 		)
 	})
-}
-
-func (c *Conveyor) WithGzipSupport(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Type") != "application/json" &&
-			r.Header.Get("Content-Type") != "text/html" {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		// Check if the request body is Gzip-encoded
-		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-			gr, err := gzip.NewReader(r.Body)
-			if err != nil {
-				http.Error(w, "Failed to decode gzip request body", http.StatusInternalServerError)
-				return
-			}
-			r.Body = gr
-			if err := gr.Close(); err != nil {
-				http.Error(w, "Failed to decode gzip request body", http.StatusInternalServerError)
-				return
-			}
-		}
-
-		// Check if the client supports Gzip encoding
-		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			c.log.Info(
-				"================== Encode ===================",
-				"header", r.Header.Get("Accept-Encoding"),
-			)
-
-			//Wrap the response writer to enable Gzip encoding
-			//w.Header().Set("Content-Encoding", "gzip")
-			gw := &gzipWriter{
-				ResponseWriter: w,
-				Writer:         gzip.NewWriter(w),
-			}
-			next.ServeHTTP(gw, r)
-			if err := gw.Writer.(*gzip.Writer).Close(); err != nil {
-				http.Error(w, "Failed to decode gzip request body", http.StatusInternalServerError)
-				return
-			}
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-type gzipWriter struct {
-	http.ResponseWriter
-	Writer io.Writer
-}
-
-func (w *gzipWriter) Write(b []byte) (int, error) {
-	return w.Writer.Write(b)
-}
-
-func (w *gzipWriter) WriteHeader(statusCode int) {
-	w.ResponseWriter.WriteHeader(statusCode)
-}
-
-func (w *gzipWriter) Header() http.Header {
-	return w.ResponseWriter.Header()
 }
