8f726e85aad5f94b24f202ca8c6e0c7803069a02
Mr-Filatik <filatov23wlad@gmail.com>
2025-03-17 10:38:23 +0400

    Fix metric test

--- tools/windows/run_test_metrictest.bat
+++ tools/windows/run_test_metrictest.bat
@@ -1,39 +1,43 @@
+echo off
+
 cd ..\..\
 cd cmd\server\
-echo "Run build for server application"
+echo Run build for server application
 go build -o server.exe main.go
-echo "Build for server application is done"
+echo Build for server application is done
 
 cd ..\..\
 cd cmd\agent\
-echo "Run build for server application"
+echo Run build for server application
 go build -o agent.exe main.go
-echo "Build for agent application is done"
+echo Build for agent application is done
 
 cd ..\..\
 
-echo "Run TestIteration1"
+echo Run TestIteration1
 metricstest-windows-amd64 -test.run=^TestIteration1$ -binary-path=cmd/server/server
 
-echo "Run TestIteration2"
+echo Run TestIteration2
 metricstest-windows-amd64 -test.v -test.run=^TestIteration2$ -binary-path=cmd/server/server -source-path=. -agent-binary-path=cmd/agent/agent -server-port 8080
 
-echo "Run TestIteration3"
+echo Run TestIteration3
 metricstest-windows-amd64 -test.v -test.run=^TestIteration3$ -binary-path=cmd/server/server -source-path=. -agent-binary-path=cmd/agent/agent -server-port 8080
 
-echo "Run TestIteration4"
+echo Run TestIteration4
 metricstest-windows-amd64 -test.v -test.run=^TestIteration4$ -binary-path=cmd/server/server -source-path=. -agent-binary-path=cmd/agent/agent -server-port 8080
 
-echo "Run TestIteration5"
+echo Run TestIteration5
 metricstest-windows-amd64 -test.v -test.run=^TestIteration5$ -agent-binary-path=cmd/agent/agent -binary-path=cmd/server/server -server-port=8081 -source-path=.
 
-echo "Run TestIteration6"
+echo Run TestIteration6
 metricstest-windows-amd64 -test.v -test.run=^TestIteration6$ -agent-binary-path=cmd/agent/agent -binary-path=cmd/server/server -server-port=$SERVER_PORT -source-path=.
 
-echo "Run TestIteration7"
+echo Run TestIteration7
 metricstest-windows-amd64 -test.v -test.run=^TestIteration7$ -agent-binary-path=cmd/agent/agent -binary-path=cmd/server/server -server-port=$SERVER_PORT -source-path=.
 
-echo "Run TestIteration8"
+echo Run TestIteration8
 metricstest-windows-amd64 -test.v -test.run=^TestIteration8$ -agent-binary-path=cmd/agent/agent -binary-path=cmd/server/server -server-port=8080 -source-path=.
 
 pause
+
+echo on
