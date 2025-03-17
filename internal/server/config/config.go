a1f0aaca77da5900e6339bb219232291bb7acdeb
Vladislav Filatov <vladislav.filatov@mego.travel>
2025-03-17 23:04:15 +0400

    Refactoring configs

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -28,44 +28,56 @@ func Initialize() *Config {
 		Restore:         defaultRestore,
 	}
 
+	config.getFlags()
+	config.getEnvironments()
+
+	return &config
+}
+
+func (c *Config) getFlags() {
 	argEndpValue := flag.String("a", defaultServerAddress, "HTTP server endpoint")
 	argIntervalValue := flag.Int64("i", defaultStoreInterval, "Interval in seconds to save data")
 	argFileValue := flag.String("f", defaultFileStoragePath, "Path to file")
 	argRestoreValue := flag.Bool("r", defaultRestore, "Loading data when the application starts")
+
 	flag.Parse()
+
 	if argEndpValue != nil && *argEndpValue != "" {
-		config.ServerAddress = *argEndpValue
+		c.ServerAddress = *argEndpValue
 	}
 	if argIntervalValue != nil && *argIntervalValue >= 0 {
-		config.StoreInterval = *argIntervalValue
+		c.StoreInterval = *argIntervalValue
 	}
 	if argFileValue != nil && *argFileValue != "" {
-		config.FileStoragePath = *argFileValue
+		c.FileStoragePath = *argFileValue
 	}
 	if argRestoreValue != nil {
-		config.Restore = *argRestoreValue
+		c.Restore = *argRestoreValue
 	}
+}
 
+func (c *Config) getEnvironments() {
 	envEndpValue, ok := os.LookupEnv("ADDRESS")
 	if ok && envEndpValue != "" {
-		config.ServerAddress = envEndpValue
+		c.ServerAddress = envEndpValue
 	}
+
 	envStoreValue, ok := os.LookupEnv("STORE_INTERVAL")
 	if ok && envStoreValue != "" {
 		if val, err := strconv.ParseInt(envStoreValue, 10, 64); err == nil {
-			config.StoreInterval = val
-		}
-	}
-	envFileValue, ok := os.LookupEnv("FILE_STORAGE_PATH")
-	if ok && envFileValue != "" {
-		config.FileStoragePath = envFileValue
-	}
-	envRestoreValue, ok := os.LookupEnv("RESTORE")
-	if ok && envRestoreValue != "" {
-		if val, err := strconv.ParseBool(envRestoreValue); err == nil {
-			config.Restore = val
+			c.StoreInterval = val
 		}
 	}
 
-	return &config
+	envFileValue, ok := os.LookupEnv("FILE_STORAGE_PATH")
+	if ok && envFileValue != "" {
+		c.FileStoragePath = envFileValue
+	}
+
+	envRestoreValue, ok := os.LookupEnv("RESTORE")
+	if ok && envRestoreValue != "" {
+		if val, err := strconv.ParseBool(envRestoreValue); err == nil {
+			c.Restore = val
+		}
+	}
 }
