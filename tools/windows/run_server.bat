344e45e39b6d0ac6553e622d47cd7ef7cf47ec09
Mr-Filatik <filatov23wlad@gmail.com>
2025-08-13 16:45:34 +0400

    chore: add trusted value for server bat file

--- tools/windows/run_server.bat
+++ tools/windows/run_server.bat
@@ -1,3 +1,3 @@
-go run ..\..\cmd\server\main.go -a=localhost:8080 -i=15 -r=false -d=postgres://user:password@localhost:5432/postgres?sslmode=disable -k=FILATIK_KEY_FOR_HASHING -crypto-key=C:\Projects\Personal\Go\go-metrics-collector\web\crypto\private.pem
+go run ..\..\cmd\server\main.go -a=localhost:8080 -i=15 -r=false -d=postgres://user:password@localhost:5432/postgres?sslmode=disable -k=FILATIK_KEY_FOR_HASHING -crypto-key=C:\Projects\Personal\Go\go-metrics-collector\web\crypto\private.pem -t=95.104.167.8
 
 pause
